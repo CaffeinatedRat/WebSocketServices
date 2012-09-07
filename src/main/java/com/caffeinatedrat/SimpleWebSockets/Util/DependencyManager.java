6f88393609714205685cb137b8975c68775df272
Ken Anderson <CaffeinatedRat@gmail.com>
2012-09-06 23:07:02 -0400

    Split the extractJar method into extractJar and isJarExtracted, as well as adding loadJar, which does nothing useful...

--- src/main/java/com/caffeinatedrat/SimpleWebSockets/Util/DependencyManager.java
+++ src/main/java/com/caffeinatedrat/SimpleWebSockets/Util/DependencyManager.java
@@ -28,6 +28,9 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.text.MessageFormat;
 
 import com.caffeinatedrat.SimpleWebSockets.Globals;
@@ -39,44 +42,47 @@ import com.caffeinatedrat.SimpleWebSockets.Globals;
  * @author CaffeinatedRat
  */
 public class DependencyManager {
+
+    public static boolean isJarExtracted(String jarName, String destination) {
+        File file = new File(destination + "/" + jarName);
+        return file.exists();
+    }
     
-    public static boolean ExtractJar(Object object, String jarName, String destination) {
+    public static boolean extractJar(Object object, String jarName, String destination) {
         
         InputStream inputStream = null;
         
         try {
             //Check if the file exists before we attempt to go through the extraction process.
             File file = new File(destination + "/" + jarName);
-            if (!file.exists())
-            {
-                Logger.info(MessageFormat.format("Unpacking {0}...", jarName));
+
+            Logger.info(MessageFormat.format("Unpacking {0}...", jarName));
                 
-                //Verify the plugin folder exists and create it if it does not.
-                File directory = new File(destination);
-                if (!directory.exists()) {
-                    if (!directory.mkdirs()) {
-                        throw new IOException(MessageFormat.format("Could not create the directory {0}.", directory.getAbsolutePath()));
-                    }
+            //Verify the plugin folder exists and create it if it does not.
+            File directory = new File(destination);
+            if (!directory.exists()) {
+                if (!directory.mkdirs()) {
+                    throw new IOException(MessageFormat.format("Could not create the directory {0}.", directory.getAbsolutePath()));
                 }
-                
-                //Create the file.
-                if(!file.exists()) {
-                    if(!file.createNewFile()) {
-                        throw new IOException(MessageFormat.format("Could not create the file {0}.", file.getAbsolutePath()));
-                    }
+            }
+            
+            //Create the file.
+            if(!file.exists()) {
+                if(!file.createNewFile()) {
+                    throw new IOException(MessageFormat.format("Could not create the file {0}.", file.getAbsolutePath()));
                 }
-                
-                //Get the jar file within the jar file.
-                inputStream = object.getClass().getResourceAsStream("/" + jarName);
-                FileOutputStream output = new FileOutputStream(file);
-                
-                //Build the jar file.
-                byte[] data = new byte[Globals.READ_CHUNK_SIZE];
-                int len = 0;
-                while (inputStream.available() > 0) {
-                    len = inputStream.read(data, 0, Globals.READ_CHUNK_SIZE);
-                    output.write(data, 0, len);
-                }
+            }
+            
+            //Get the jar file within the jar file.
+            inputStream = object.getClass().getResourceAsStream("/" + jarName);
+            FileOutputStream output = new FileOutputStream(file);
+            
+            //Build the jar file.
+            byte[] data = new byte[Globals.READ_CHUNK_SIZE];
+            int len = 0;
+            while (inputStream.available() > 0) {
+                len = inputStream.read(data, 0, Globals.READ_CHUNK_SIZE);
+                output.write(data, 0, len);
             }
 
             return true;
@@ -97,5 +103,26 @@ public class DependencyManager {
         }
 
        return false;
+    }
+    
+    public static boolean loadJar(String jarPath, String className) {
+    
+        try {
+ 
+            URL url = new URL(jarPath);
+            URL[] urls = new URL[]{url};
+            ClassLoader cl = new URLClassLoader(urls);
+            Class cls = cl.loadClass("net.iharder.Base64");
+            
+            return true;
+        }
+        catch (ClassNotFoundException cnfe){
+        
+        }
+        catch (MalformedURLException me) {
+            
+        }
+        
+        return false;
     }
 }
