8dd8562e9e6f11196095caf3d7d8087073af39c9
CaffeinatedRat <caffeinatedrat@gmail.com>
2014-01-05 19:40:49 -0500

    Copyright extended.

--- src/main/java/com/caffeinatedrat/SimpleWebSockets/Connection.java
+++ src/main/java/com/caffeinatedrat/SimpleWebSockets/Connection.java
@@ -1,5 +1,5 @@
 /**
-* Copyright (c) 2012-2013, Ken Anderson <caffeinatedrat at gmail dot com>
+* Copyright (c) 2012-2014, Ken Anderson <caffeinatedrat at gmail dot com>
 * All rights reserved.
 * Redistribution and use in source and binary forms, with or without
 * modification, are permitted provided that the following conditions are met:
